b21ea6ee5dbb192c8ef43980a1a871d9a3f74c60
Haruka <45176998+haruka98@users.noreply.github.com>
2021-08-25 20:25:01 +0200

    Create v7_template.sql

---
+++ templates/v7_template.sql
@@ -0,0 +1,62 @@
+--
+-- Aura Kingdom v7 Paragon Table
+--
+
+-- Columns:
+-- lottery_id, category, drop_level, weekday, level_order, item_id, max_stack, drop_rate, notify, get_only, shining_hint, jack_pot
+
+-- First row
+INSERT INTO "lottery" VALUES (1, 0, 1, 0, 1, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (2, 0, 1, 0, 2, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (3, 0, 1, 0, 3, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (4, 0, 1, 0, 4, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (5, 0, 1, 0, 5, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (6, 0, 1, 0, 6, 00000, 0, 0, 0, 0, 0, 0);
+
+-- Second row
+INSERT INTO "lottery" VALUES (7, 0, 2, 0, 1, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (8, 0, 2, 0, 2, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (9, 0, 2, 0, 3, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (10, 0, 2, 0, 4, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (11, 0, 2, 0, 5, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (12, 0, 2, 0, 6, 00000, 0, 0, 0, 0, 0, 0);
+
+-- Third row
+INSERT INTO "lottery" VALUES (13, 0, 3, 0, 1, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (14, 0, 3, 0, 2, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (15, 0, 3, 0, 3, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (16, 0, 3, 0, 4, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (17, 0, 3, 0, 5, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (18, 0, 3, 0, 6, 00000, 0, 0, 0, 0, 0, 0);
+
+-- Fourth row
+INSERT INTO "lottery" VALUES (19, 0, 4, 0, 1, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (20, 0, 4, 0, 2, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (21, 0, 4, 0, 3, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (22, 0, 4, 0, 4, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (23, 0, 4, 0, 5, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (24, 0, 4, 0, 6, 00000, 0, 0, 0, 0, 0, 0);
+
+-- Fifth row
+INSERT INTO "lottery" VALUES (25, 0, 5, 0, 1, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (26, 0, 5, 0, 2, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (27, 0, 5, 0, 3, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (28, 0, 5, 0, 4, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (29, 0, 5, 0, 5, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (30, 0, 5, 0, 6, 00000, 0, 0, 0, 0, 0, 0);
+
+-- Sixth row
+INSERT INTO "lottery" VALUES (31, 0, 6, 0, 1, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (32, 0, 6, 0, 2, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (33, 0, 6, 0, 3, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (34, 0, 6, 0, 4, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (35, 0, 6, 0, 5, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (36, 0, 6, 0, 6, 00000, 0, 0, 0, 0, 0, 0);
+
+-- Seventh row
+INSERT INTO "lottery" VALUES (37, 0, 7, 0, 1, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (38, 0, 7, 0, 2, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (39, 0, 7, 0, 3, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (41, 0, 7, 0, 5, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (42, 0, 7, 0, 6, 00000, 0, 0, 0, 0, 0, 0);
+INSERT INTO "lottery" VALUES (40, 0, 7, 0, 4, 00000, 0, 0, 0, 0, 0, 0);
